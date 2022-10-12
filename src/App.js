16db92f411e6380f5adf51f77fadb724c9e1133f
Taqinur <taqi1662@gmail.com>
2022-10-12 23:50:26 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+    const newData = data.data;
+    console.log(newData);
+    const {total} = newData;
     return (
         <div>
-            <h2>Available Statictics</h2>
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart width={200} height={100} data={newData}>
+                <Bar dataKey="total" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
